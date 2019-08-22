6020fefc33a53dd8f267ff2c5621f69515639aaa
mikimoyal <mikimoyal@gmail.com>
2019-08-22 14:51:58 -0400

    Add files via upload

--- scripts/TableModel.js
+++ scripts/TableModel.js
@@ -1,23 +1,25 @@
-﻿var TableModel = {	
-                
+﻿var TableModel = {
+
     table:{
 		deck: [],
 		flop: [],
 		turn: new Object(),
 		river: new Object(),
 		p1Cards: [],
-		p2Cards: []	
+		p2Cards: [],
+    p3Cards: [],
+		p4Cards: []
 	},
-	
+
 	suits: new Array("Clubs","Spades","Hearts","Diamonds"),
 	cardNames: new Array("Ace","King","Queen","Jack","Ten","Nine","Eight","Seven","Six","Five","Four","Three","Two"),
-	
+
 	buildDeck: function(){
 		var value= 1;
 		var rank = 0;
 		var self =this;
 		$.each(self.cardNames, function(x){
-			$.each(self.suits, function(y){				
+			$.each(self.suits, function(y){
 				mod = y % 4;
 				if(mod == 0)
 					rank+=1;
@@ -25,8 +27,8 @@
 				value +=1;
 			});
 		});
-		
-	},	
+
+	},
 	newCard: function(cardName, s, value, rank){
 			var card = new Object();
 			card.name= cardName+" of "+ s;
@@ -36,23 +38,27 @@
 			card.rank = rank;
 			card.isDealt = false;
 			card.image = "images/"+value + ".png"
-			
-			return card;			
+
+			return card;
 		},
-		
+
 	deal: function(){
 		var self = this;
 		self.table.p1Cards.push(self.getCard());
 		self.table.p1Cards.push(self.getCard());
 		self.table.p2Cards.push(self.getCard());
 		self.table.p2Cards.push(self.getCard());
+    self.table.p3Cards.push(self.getCard());
+		self.table.p3Cards.push(self.getCard());
+    self.table.p4Cards.push(self.getCard());
+		self.table.p4Cards.push(self.getCard());
 		self.table.flop.push(self.getCard());
 		self.table.flop.push(self.getCard());
 		self.table.flop.push(self.getCard());
 		self.table.turn=self.getCard();
-		self.table.river=self.getCard();		
+		self.table.river=self.getCard();
 	},
-	
+
 	getCard: function(){
 		var self = this;
 		var randomnumber = Math.floor(Math.random()*52);
@@ -60,18 +66,20 @@
 		if(card.isDealt)
 			card = self.getCard();
 		card.isDealt = true;
-		return card;	
-		
+		return card;
+
 	},
-		
-	shuffle: function(){	
+
+	shuffle: function(){
 		this.table.deck=[];
 		this.table.flop=[];
 		this.table.turn=new Object();
 		this.table.river=new Object();
 		this.table.p1Cards=[];
 		this.table.p2Cards=[];
-		this.buildDeck();			
+    this.table.p3Cards=[];
+		this.table.p4Cards=[];
+		this.buildDeck();
 	},
 	flopResult: function(cards){
 		var self = this;
@@ -81,26 +89,26 @@
 		if(cards[0].rank == cards[1].rank)
 			{
 				count+=1;
-				resultMessage += self.checkCard(cards[0],count,board);				
+				resultMessage += self.checkCard(cards[0],count,board);
 			}
 		else{
 			$.each(cards, function(i){
 				count = 0;
 				resultMessage += self.checkCard(cards[i],count,board);
-			});		
+			});
 		}
-		
+
 		resultMessage += self.checkBoard(cards, board);
-		
+
 		var flushMessage = self.checkFlush(cards, board);
-		
+
 		if (flushMessage != "")
 			resultMessage = flushMessage;
-		
-		if(resultMessage==""){		
-			resultMessage = self.getHighCard(cards, board);			
+
+		if(resultMessage==""){
+			resultMessage = self.getHighCard(cards, board);
 		}
-		
+
 		return resultMessage;
 	},
 	turnResult: function(cards){
@@ -112,11 +120,11 @@
 				board.push(self.table.flop[i]);
 		});
 		board.push(self.table.turn);
-		
+
 		if(cards[0].rank == cards[1].rank)
 			{
 				count+=1;
-				resultMessage += self.checkCard(cards[0],count,board);				
+				resultMessage += self.checkCard(cards[0],count,board);
 			}
 		else{
 			$.each(cards, function(i){
@@ -124,14 +132,14 @@
 			});
 		}
 		resultMessage += self.checkBoard(cards, board);
-		
+
 		var flushMessage = self.checkFlush(cards, board);
-		
+
 		if (flushMessage != "")
 			resultMessage = flushMessage;
-		
-		if(resultMessage==""){		
-			resultMessage = self.getHighCard(cards, board);			
+
+		if(resultMessage==""){
+			resultMessage = self.getHighCard(cards, board);
 		}
 		return resultMessage;
 	},
@@ -145,36 +153,36 @@
 		});
 		board.push(self.table.turn);
 		board.push(self.table.river);
-		
-		
+
+
 		if(cards[0].rank == cards[1].rank)
 			{
 				count+=1;
-				resultMessage += self.checkCard(cards[0],count,board);				
+				resultMessage += self.checkCard(cards[0],count,board);
 			}
 		else{
 			$.each(cards, function(i){
-				
+
 				resultMessage += self.checkCard(cards[i],count,board);
 			});
 		}
 		resultMessage += self.checkBoard(cards, board);
-		
+
 		var flushMessage = self.checkFlush(cards, board);
-		
+
 		if (flushMessage != "")
 			resultMessage = flushMessage;
-		
-		if(resultMessage==""){		
-			resultMessage = self.getHighCard(cards, board);			
+
+		if(resultMessage==""){
+			resultMessage = self.getHighCard(cards, board);
 		}
 		return resultMessage;
 	},
 	checkCard: function(card, count, board){
 				var resultMessage = "";
 				count += $.grep(board, function(c){return c.rank == card.rank}).length;
-							
-				
+
+
 				if(count ==1){
 					resultMessage += " Pair of " + card.valueName + "s";
 				}
@@ -186,28 +194,28 @@
 				}
 				return resultMessage;
 	},
-	
+
 	checkBoard: function(cards, board){
 				var resultMessage = "";
 				var matches = [];
 				var temp = [];
 				$.each(board, function(i){temp.push(board[i])});
-				$.each(temp, function(i){				
-						if(temp[i].rank != cards[0].rank && temp[i].rank != cards[1].rank ){								
+				$.each(temp, function(i){
+						if(temp[i].rank != cards[0].rank && temp[i].rank != cards[1].rank ){
 								var match = $.grep(temp, function(c){return c.rank == temp[i].rank});
-								
+
 								if(match.length >1 )
 								{	//this will check to make sure our paired cards only gets into the matches array once
 									var checkMatchesLength = $.grep(matches, function(c){return c.rank== match[0].rank}).length;
 									if(checkMatchesLength <1)
-										matches.push(match[0]);		
-								}											
-						}				
+										matches.push(match[0]);
+								}
+						}
 				});
-				
+
 				$.each(matches, function(i){
 						var count = $.grep(temp, function(c){return c.rank == matches[i].rank}).length;
-						
+
 						if(count ==2){
 							resultMessage += " Pair of " + matches[i].valueName + "s";
 						}
@@ -216,27 +224,27 @@
 						}
 						if(count ==4){
 							resultMessage += " Four " + matches[i].valueName+ "s";
-						}						
+						}
 				});
 				return resultMessage;
 	},
-	
+
 	getHighCard: function(cards, board){
 		var resultMessage = "";
 		var hand = [];
 		$.each(board, function(i){hand.push(board[i]);});
 		$.each(cards, function(i){hand.push(cards[i]);});
-			
+
 			return this.highCard(hand);
-	
+
 	},
-	
+
 	highCard: function(hand){
-	
+
 			var min = hand[0].rank;
 			var card = hand[0];
 			var len = hand.length;
-			for (var i = 1; i < len; i++){ 
+			for (var i = 1; i < len; i++){
 				if (hand[i].rank < min){
 					min = hand[i].rank;
 					card = hand[i];
@@ -245,23 +253,23 @@
 			resultMessage = " " +card.valueName + " High";
 			return  resultMessage;
 	},
-	
+
 	checkFlush: function(cards, board){
 		var hand = [];
 		var self = this;
 		$.each(board, function(i){hand.push(board[i]);});
 		$.each(cards, function(i){hand.push(cards[i]);});
-		
+
 		var suited = $.grep(hand, function(c) {return c.suit== cards[0].suit});
-		
+
 		if(suited.length > 4)
 			return self.highCard(suited) + " " + cards[0].suit + " Flush";
 		var suited2 = $.grep(hand, function(c) {return c.suit== cards[1].suit});
-		
+
 		if(suited2.length > 4)
 			return self.highCard(suited2) + " " + cards[0].suit + " Flush";
-		
+
 		return "";
 	}
-	
+
 }
--- scripts/TexasHoldEmPageFunctions.js
+++ scripts/TexasHoldEmPageFunctions.js
@@ -6,9 +6,13 @@
 		$("#p1card2").attr("src", TableModel.table.p1Cards[1].image);
 		$("#p2card1").attr("src", TableModel.table.p2Cards[0].image);
 		$("#p2card2").attr("src", TableModel.table.p2Cards[1].image);
+    $("#p3card1").attr("src", TableModel.table.p3Cards[0].image);
+		$("#p3card2").attr("src", TableModel.table.p3Cards[1].image);
+    $("#p4card1").attr("src", TableModel.table.p4Cards[0].image);
+		$("#p4card2").attr("src", TableModel.table.p4Cards[1].image);
   };
   var flop = function(){
-		
+
 		$("#flop-button").toggle();
 		$("#turn-button").toggle();
 		$("#flop1").attr("src", TableModel.table.flop[0].image);
@@ -16,21 +20,27 @@
 		$("#flop3").attr("src", TableModel.table.flop[2].image);
 		$("#p1-hand").attr("innerHTML" ,"Player 1 Has:" +TableModel.flopResult(TableModel.table.p1Cards));
 		$("#p2-hand").attr("innerHTML" ,"Player 2 Has:" +TableModel.flopResult(TableModel.table.p2Cards));
+    $("#p3-hand").attr("innerHTML" ,"Player 3 Has:" +TableModel.flopResult(TableModel.table.p3Cards));
+		$("#p4-hand").attr("innerHTML" ,"Player 4 Has:" +TableModel.flopResult(TableModel.table.p4Cards));
   };
-  var turn1 = function(){		
+  var turn1 = function(){
 		$("#turn-button").toggle();
 		$("#river-button").toggle();
 		$("#turn").attr("src", TableModel.table.turn.image);
 		$("#p1-hand").attr("innerHTML" ,"Player 1 Has:" +TableModel.turnResult(TableModel.table.p1Cards));
 		$("#p2-hand").attr("innerHTML" ,"Player 2 Has:" +TableModel.turnResult(TableModel.table.p2Cards));
+    $("#p3-hand").attr("innerHTML" ,"Player 3 Has:" +TableModel.turnResult(TableModel.table.p3Cards));
+		$("#p4-hand").attr("innerHTML" ,"Player 4 Has:" +TableModel.turnResult(TableModel.table.p4Cards));
   };
   var river1 = function(){
-		
+
 		$("#river-button").toggle();
 		$("#shuffle-button").toggle();
 		$("#river").attr("src", TableModel.table.river.image);
 		$("#p1-hand").attr("innerHTML" ,"Player 1 Has:" +TableModel.riverResult(TableModel.table.p1Cards));
 		$("#p2-hand").attr("innerHTML" ,"Player 2 Has:" +TableModel.riverResult(TableModel.table.p2Cards));
+    $("#p3-hand").attr("innerHTML" ,"Player 3 Has:" +TableModel.riverResult(TableModel.table.p3Cards));
+		$("#p4-hand").attr("innerHTML" ,"Player 4 Has:" +TableModel.riverResult(TableModel.table.p4Cards));
   };
   var shuffle = function(){
 		TableModel.shuffle();
@@ -45,6 +55,12 @@
 		$("#p1card2").attr("src", "images/b2fv.png");
 		$("#p2card1").attr("src", "images/b2fv.png");
 		$("#p2card2").attr("src", "images/b2fv.png");
+    $("#p3card1").attr("src", "images/b2fv.png");
+		$("#p3card2").attr("src", "images/b2fv.png");
+    $("#p4card1").attr("src", "images/b2fv.png");
+		$("#p4card2").attr("src", "images/b2fv.png");
 		$("#p1-hand").attr("innerHTML","");
 		$("#p2-hand").attr("innerHTML","");
+    $("#p3-hand").attr("innerHTML","");
+		$("#p4-hand").attr("innerHTML","");
   };
